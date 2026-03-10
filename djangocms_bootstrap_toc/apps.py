8f2f295aa72b26fa7855ff6cdc66dbb8ce89d343
carderm <mc.github@michaelcarder.co.uk>
2026-03-10 23:45:07 +1100

    Add .idea/ to .gitignore

--- djangocms_bootstrap_toc/apps.py
+++ djangocms_bootstrap_toc/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class DjangoCMSBootstrapTOCConfig(AppConfig):
     name = 'djangocms_bootstrap_toc'
     verbose_name = 'Django CMS Table of Contents'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         pass
